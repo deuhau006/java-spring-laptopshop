3bad35d98ad98b27b9a667ef8e3803956710a969
deuhau006 <caodanh230602@gmail.com>
2024-12-11 14:00:11 +0700

    'init'

---
+++ src/main/java/com/example/java_spring_laptopshop/JavaSpringLaptopshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLaptopshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLaptopshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_spring_laptopshop/JavaSpringLaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
